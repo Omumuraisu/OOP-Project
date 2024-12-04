cf6d5f473fbcbacefff52838bb2249e1e3275f0a
Meryll Klaryze Polaron <167072242+k00617500@users.noreply.github.com>
2024-12-04 13:46:27 +0800

    Update User-Updated.java

--- User-Updated.java
+++ User-Updated.java
@@ -1,89 +1,89 @@
 import java.util.Scanner;
 
-// Abstract class for User Roles
-abstract class UserRole {
-    protected String roleName;
+// Abstract class for User Types
+abstract class UserType {
+    protected String userName;
 
-    public UserRole(String roleName) {
-        this.roleName = roleName;
+    public UserType(String userName) {
+        this.userName = userName;
     }
 
-    public abstract void displayRoleDetails();
+    public abstract void displayTypeDetails();
 }
 
 // Facilitator 
-class Facilitator extends UserRole {
+class Facilitator extends UserType {
     public Facilitator() {
         super("Facilitator");
     }
 
     @Override
-    public void displayRoleDetails() {
-        System.out.println("Role: " + roleName + " - Facilitates events and activities.");
+    public void displayTypeDetails() {
+        System.out.println("Type: " + userName + " - Facilitates events and activities.");
     }
 }
 
 // Donor 
-class Donor extends UserRole {
+class Donor extends UserType {
     public Donor() {
         super("Donor");
     }
 
     @Override
-    public void displayRoleDetails() {
-        System.out.println("Role: " + roleName + " - Provides financial or material support.");
+    public void displayTypeDetails() {
+        System.out.println("Type: " + userName + " - Provides financial or material support.");
     }
 }
 
 // Volunteer 
-class Volunteer extends UserRole {
+class Volunteer extends UserType {
     public Volunteer() {
         super("Volunteer");
     }
 
     @Override
-    public void displayRoleDetails() {
-        System.out.println("Role: " + roleName + " - Participates in on-ground activities.");
+    public void displayTypeDetails() {
+        System.out.println("Type: " + userName + " - Participates in on-ground activities.");
     }
 }
 
 // Log-in Class
 class LoginUser {
     private String userName;
-    private String password;
-    private UserRole role;
+    private String userPassword;
+    private UserType userType;
 
-    public void setLogin(String userName, String password, UserRole role) {
+    public void setLogin(String userName, String userPassword, UserType userType) {
         this.userName = userName;
-        this.password = password;
-        this.role = role;
+        this.userPassword = userPassword;
+        this.userType = userType;
     }
 
     public String getUsername() {
         return userName;
     }
 
-    public UserRole getRole() {
-        return role;
+    public UserType getType() {
+        return userType;
     }
 
     public void displayLogin() {
         System.out.println("Username: " + userName);
-        role.displayRoleDetails();
+        userType.displayTypeDetails();
     }
 }
 
 // Sign Up Subclass
 class SignUp extends LoginUser {
-    private String contact;
+    private String userContact;
 
-    public SignUp(String userName, String password, String contact, UserRole role) {
-        this.setLogin(userName, password, role);
-        this.contact = contact;
+    public SignUp(String userName, String userPassword, String userContact, UserType userType) {
+        this.setLogin(userName, userPassword, userType);
+        this.userContact = userContact;
     }
 
     public String getContact() {
-        return contact;
+        return userContact;
     }
 }
 
@@ -93,7 +93,7 @@ public class User {
         Scanner userInput = new Scanner(System.in);
 
         System.out.println(" ");
-        System.out.println("          |===== 1. Login =====|                 |===== 2. Sign up =====|");
+        System.out.println("          |===== 1. Login =====|                   |===== 2. Sign up =====|");
         System.out.println(" ");
         System.out.print("Enter your choice: ");
         int option = userInput.nextInt();
@@ -106,11 +106,11 @@ public class User {
                 System.out.print("Enter Password: ");
                 String loginPass = userInput.next();
 
-                UserRole loginRole = selectRole(userInput);
-                if (loginRole == null) break;
+                UserType loginType = selectType(userInput);
+                if (loginType == null) break;
 
                 LoginUser loginUser = new LoginUser();
-                loginUser.setLogin(loginName, loginPass, loginRole);
+                loginUser.setLogin(loginName, loginPass, loginType);
 
                 System.out.println("\nLogin Successful!");
                 loginUser.displayLogin();
@@ -135,14 +135,14 @@ public class User {
                     }
                 }
 
-                UserRole signUpRole = selectRole(userInput);
-                if (signUpRole == null) break;
+                UserType signUpType = selectType(userInput);
+                if (signUpType == null) break;
 
-                SignUp signUpUser = new SignUp(signUpName, signUpPass, contact, signUpRole);
+                SignUp signUpUser = new SignUp(signUpName, signUpPass, contact, signUpType);
 
                 System.out.println("\nSign-Up Successful!");
                 System.out.println("Username: " + signUpUser.getUsername());
-                signUpUser.getRole().displayRoleDetails();
+                signUpUser.getType().displayTypeDetails();
                 System.out.println("Contact Number: " + signUpUser.getContact());
                 break;
 
@@ -154,15 +154,15 @@ public class User {
         userInput.close();
     }
 
-    public static UserRole selectRole(Scanner userInput) {
-        System.out.println("\nSelect Your Role:");
+    public static UserType selectType(Scanner userInput) {
+        System.out.println("\nSelect Your Type:");
         System.out.println("1. Facilitator");
         System.out.println("2. Donor");
         System.out.println("3. Volunteer");
-        System.out.print("Enter your role (1/2/3): ");
-        int roleChoice = userInput.nextInt();
+        System.out.print("Enter your type (1/2/3): ");
+        int typeChoice = userInput.nextInt();
 
-        switch (roleChoice) {
+        switch (typeChoice) {
             case 1:
                 return new Facilitator();
             case 2:
@@ -170,7 +170,7 @@ public class User {
             case 3:
                 return new Volunteer();
             default:
-                System.out.println("Invalid role selected. Please restart.");
+                System.out.println("Invalid type selected. Please restart.");
                 return null;
         }
     }
