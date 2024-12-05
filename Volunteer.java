16306c5b67b9a468d03413b3cd94e46a840cba20
Yahn-ian <iannayzabel.beray@wvsu.edu.ph>
2024-12-05 22:24:48 +0800

    Add files via upload

---
+++ Volunteer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Volunteer {
+
+    private String volunteerName;
+    private String volunteerLocation;
+    private int volunteerContact;
+    private int volunteerType;
+
+    public void setVolunteerDetails (String volunteerName, String volunteerLocation,int volunteerContact, int volunteerType ){
+        this.volunteerName = volunteerName;
+        this.volunteerLocation = volunteerLocation;
+        this.volunteerContact = volunteerContact;
+        this.volunteerType = volunteerType;
+    }
+}
+
